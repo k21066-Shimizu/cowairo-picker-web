c08cfff99387a60c190cd28dddcd3b4429b98bc9
k21066-Shimizu <k21066kk@aitech.ac.jp>
2024-11-10 13:46:34 +0900

    add: install chakra ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Provider } from "@/components/ui/provider";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -24,9 +25,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html suppressHydrationWarning>
+      <body>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
