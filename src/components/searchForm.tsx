d22b4fe94f9aebbc6a2f6168444d6426780ff249
k21066-Shimizu <k21066kk@aitech.ac.jp>
2024-11-10 19:12:48 +0900

    add: react-hook-form

--- src/components/searchForm.tsx
+++ src/components/searchForm.tsx
@@ -1,20 +1,51 @@
+"use client";
+
+import { Field } from "@/components/ui/field";
 import { For, HStack } from "@chakra-ui/react";
+import { Controller, useForm } from "react-hook-form";
 import SearchSlider from "./searchSlider";
 
 const SCORE_LABELS = [
-  { name: "声の性別", low: "女性的", high: "男性的" },
-  { name: "滑舌", low: "舌足らず", high: "はきはき" },
-  { name: "特有性", low: "素直", high: "癖がある" },
-  { name: "声の年齢", low: "幼い", high: "大人びた" },
-  { name: "透明感", low: "ノイジー", high: "クリア" },
-  { name: "声の強さ", low: "優しい", high: "力強い" },
-  { name: "声の明度", low: "暗い", high: "明るい" },
-];
+  { id: "gen", name: "声の性別", low: "女性的", high: "男性的" },
+  { id: "lsn", name: "滑舌", low: "舌足らず", high: "はきはき" },
+  { id: "unq", name: "特有性", low: "素直", high: "癖がある" },
+  { id: "age", name: "声の年齢", low: "幼い", high: "大人びた" },
+  { id: "clr", name: "透明感", low: "ノイジー", high: "クリア" },
+  { id: "pow", name: "声の強さ", low: "優しい", high: "力強い" },
+  { id: "brt", name: "声の明度", low: "暗い", high: "明るい" },
+] as const;
 
 export default function SearchForm() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Record<(typeof SCORE_LABELS)[number]["id"], number[]>>({
+    defaultValues: { gen: [3], lsn: [3], unq: [3], age: [3], clr: [3], pow: [3], brt: [3] },
+  });
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
-    <HStack maxW={"2xl"} w={"100%"} justify={"space-around"}>
-      <For each={SCORE_LABELS}>{(label) => <SearchSlider key={label.name} label={label} />}</For>
+    <HStack as={"form"} onSubmit={onSubmit} maxW={"2xl"} w={"100%"} justify={"space-around"}>
+      <For each={SCORE_LABELS}>
+        {(label) => (
+          <Controller
+            key={label.id}
+            name={label.id}
+            control={control}
+            render={({ field }) => (
+              <Field
+                label={`Slider: ${field.value[0]}`}
+                invalid={!!errors[label.id]?.length}
+                errorText={errors[label.id]?.[0]?.message}
+              >
+                <SearchSlider label={label} field={field} />
+              </Field>
+            )}
+          />
+        )}
+      </For>
     </HStack>
   );
 }
--- src/components/searchSlider.tsx
+++ src/components/searchSlider.tsx
@@ -1,18 +1,39 @@
 import { Slider } from "@/components/ui/slider";
 import { VStack } from "@chakra-ui/react";
+import { ControllerRenderProps } from "react-hook-form";
 
 type Props = {
   label: { name: string; low: string; high: string };
+  field: ControllerRenderProps<
+    Record<"gen" | "lsn" | "unq" | "age" | "clr" | "pow" | "brt", number[]>,
+    "gen" | "lsn" | "unq" | "age" | "clr" | "pow" | "brt"
+  >;
 };
 
 export default function SearchSlider(props: Props) {
-  const { label } = props;
+  const { label, field } = props;
 
   return (
     <VStack>
       <p>{label.name}</p>
       <p>{label.high}</p>
-      <Slider height={"200px"} orientation="vertical" defaultValue={[40]} />
+      <Slider
+        height={"200px"}
+        orientation="vertical"
+        defaultValue={[4]}
+        max={7}
+        min={1}
+        step={0.1}
+        onFocusChange={({ focusedIndex }) => {
+          if (focusedIndex !== -1) return;
+          field.onBlur();
+        }}
+        name={field.name}
+        value={field.value}
+        onValueChange={({ value }) => {
+          field.onChange(value);
+        }}
+      />
       <p>{label.low}</p>
     </VStack>
   );
