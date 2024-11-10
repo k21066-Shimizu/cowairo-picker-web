be091aa147e9b1a0784e5493029fb145c6380d0b
k21066-Shimizu <k21066kk@aitech.ac.jp>
2024-11-11 00:36:31 +0900

    add: 検索結果のスコアと差分表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
 "use client";
 
 import SearchForm from "@/components/searchForm";
+import VoicebankCard from "@/components/voicebankCard";
 import { useVoicebanks } from "@/hooks/useVoicebanks";
 import { Stack } from "@chakra-ui/react";
 
 export default function Home() {
-  const { voicebanks, loading, setScores } = useVoicebanks();
+  const { voicebanks, loading, setScores, scores } = useVoicebanks();
 
   return (
     <>
       <SearchForm setScores={setScores} />
-      <Stack gap={4} dir={"row"} wrap={"wrap"}>
+      <Stack gap={4} dir="row" w={"full"} alignItems={"center"}>
         {loading ? (
           <div>Loading...</div>
         ) : (
-          voicebanks.slice(0, 10).map((voicebank) => (
-            <div key={voicebank.charactor_name + voicebank.version}>
-              <h2>{voicebank.charactor_name}</h2>
-              <p>{voicebank.furigana}</p>
-              <p>{voicebank.version}</p>
-            </div>
-          ))
+          voicebanks
+            .slice(0, 10)
+            .map((voicebank) => (
+              <VoicebankCard
+                key={voicebank.charactor_name + voicebank.version}
+                voicebank={voicebank}
+                scores={scores}
+              />
+            ))
         )}
       </Stack>
     </>
---
+++ src/components/voicebankCard.tsx
@@ -0,0 +1,51 @@
+import { SCORE_LABELS, ScoreDataItem, ScoreId } from "@/consts/scores";
+import { Card, For, Table } from "@chakra-ui/react";
+
+type Props = {
+  voicebank: ScoreDataItem & { dist: number };
+  scores: Partial<Record<ScoreId, [number]>>;
+};
+
+export default function VoicebankCard(props: Props) {
+  const { voicebank, scores } = props;
+
+  return (
+    <Card.Root w={"2xl"} p={4}>
+      <Card.Header>
+        <Card.Title>{voicebank.charactor_name}</Card.Title>
+        <Card.Description>{voicebank.version}</Card.Description>
+      </Card.Header>
+      <Card.Body>
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <For each={SCORE_LABELS}>
+                {(label) => <Table.ColumnHeader key={label.id}>{label.name}</Table.ColumnHeader>}
+              </For>
+            </Table.Row>
+          </Table.Header>
+          <Table.Row>
+            <For each={SCORE_LABELS}>
+              {(label) => <Table.Cell key={label.id}>{voicebank[label.id]}</Table.Cell>}
+            </For>
+          </Table.Row>
+          <Table.Row>
+            <For each={SCORE_LABELS}>
+              {({ id }) => (
+                <Table.Cell key={id}>{formatScoreDiff(scores[id]?.[0], voicebank[id])}</Table.Cell>
+              )}
+            </For>
+          </Table.Row>
+        </Table.Root>
+      </Card.Body>
+    </Card.Root>
+  );
+}
+
+const formatScoreDiff = (from: number | undefined, to: number | undefined) => {
+  if (from == null || to == null) return "-";
+  const diff = to - from;
+  const diffStr = diff.toFixed(1);
+  if (diffStr === "0.0") return "±0";
+  return diff > 0 ? `+${diffStr}` : diffStr;
+};
